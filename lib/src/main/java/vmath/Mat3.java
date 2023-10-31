47d4e8d09516d7efca1f662fa81752600024c9b0
Aikokul Tashpulatova <tashpulatova_a@auca.kg>
2023-10-31 13:59:50 +0100

    Pass all the tests

--- lib/src/main/java/vmath/Mat3.java
+++ lib/src/main/java/vmath/Mat3.java
@@ -1,7 +1,11 @@
 package vmath;
 
 public class Mat3 {
-    float[] m;
+    final float[] m;
     
-    public Mat3() {}
+    public Mat3() {
+        m = new float[]{1.0f, 0.0f, 0.0f,
+                        0.0f, 1.0f, 0.0f,
+                        0.0f, 0.0f, 1.0f};
+    }
 }
